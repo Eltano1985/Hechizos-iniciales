044c082f06d5f601212144b124914d909f81a6f7
PC 6 <>
2024-04-18 17:46:24 -0600

    ejerciciosIterativos

--- Hechizos nivel 3 - Iterativo/1 - for/ej1.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej1.js
@@ -2,18 +2,51 @@ const leer = require("prompt-sync")();
 
 /**
  * Ej 1
- * Es crucial que los estudiantes aprendan los nombres de los hechizos correctamente para realizar magia de manera efectiva. En la clase de Encantamientos, los estudiantes deben demostrar su habilidad para recordar y lanzar los hechizos correctamente. Ayuda a simular este proceso creando un programa que solicite al usuario el nombre de un hechizo y le dé un número limitado de intentos para ingresarlo correctamente. Si el usuario ingresa el nombre del hechizo correctamente, el programa lo felicitará y finalizará. Si el usuario no logra ingresar el hechizo correctamente después de los intentos permitidos, el programa lo informará y finalizará.
+ * Es crucial que los estudiantes aprendan los nombres de los hechizos correctamente para realizar magia de manera efectiva. 
+ * En la clase de Encantamientos, los estudiantes deben demostrar su habilidad para recordar y lanzar los hechizos correctamente. 
+ * Ayuda a simular este proceso creando un programa que solicite al usuario el nombre de un hechizo y le dé un número limitado de intentos para ingresarlo correctamente. 
+ * Si el usuario ingresa el nombre del hechizo correctamente, el programa lo felicitará y finalizará. 
+ * Si el usuario no logra ingresar el hechizo correctamente después de los intentos permitidos, el programa lo informará y finalizará.
  * 
  * 
  * Por ejemplo: hechizo "Wingardium leviosa", cantidad de intentos maximos 4
- * 
+ *  Expecto Patronum
+        Wingardium Leviosa
+
+    Expelliarmus
  * Extra: permitir que el programa acepte 3 hechizos diferentes
  */
 
 
 function main() {
-    
+
+    let NombreDelHechizo = 0;
+
+    for (let contador = 0; contador < 4; contador++) {
+        console.log("Ingrese el nombre del hechizo");
+        NombreDelHechizo = leer();
+
+        if (NombreDelHechizo == "Expecto Patronum" || NombreDelHechizo == "Wingardium Leviosa" || NombreDelHechizo == "Expelliarmus") {
+            console.log("Felicitaciones, acertaste");
+            contador = 5
+
+
+        } else {
+            console.log("Segui Intentando");
+        }
+
+    }
 }
 
 
-main();
+
+
+
+
+
+
+
+
+
+
+    main();
--- Hechizos nivel 3 - Iterativo/1 - for/ej2.js
+++ Hechizos nivel 3 - Iterativo/1 - for/ej2.js
@@ -2,7 +2,12 @@ const leer = require("prompt-sync")();
 
 /**
  * Ej 2
- * Los Dementores son criaturas oscuras que se alimentan de la felicidad de las personas, dejándolas con recuerdos dolorosos y desesperación. Para protegerse de los Dementores, los estudiantes de Hogwarts aprenden el hechizo Patronus, que repele a estas criaturas y los aleja con recuerdos felices. En este ejercicio, simularemos un encuentro con un Dementor en un mini juego donde un estudiante debe lanzar el hechizo Patronus correctamente para repeler al Dementor y proteger su felicidad. Dependiendo de la cantidad de vida del estudiante, se necesitará ingresar el hechizo Patronus varias veces para vencer al Dementor. Si el estudiante no ingresa el hechizo correctamente, perderá turnos y el Dementor consumirá su felicidad y recuerdos felices, acercándose más a la derrota del estudiante. En otro caso si el estudiante ingresa bien los hechizos y logra vencer al dementor entonces se mostrar un mensaje de victoria
+ * Los Dementores son criaturas oscuras que se alimentan de la felicidad de las personas, dejándolas con recuerdos dolorosos y desesperación. 
+ * Para protegerse de los Dementores, los estudiantes de Hogwarts aprenden el hechizo Patronus, que repele a estas criaturas y los aleja con recuerdos felices. 
+ * En este ejercicio, simularemos un encuentro con un Dementor en un mini juego donde un estudiante debe lanzar el hechizo Patronus correctamente para repeler al Dementor y proteger su felicidad. 
+ * Dependiendo de la cantidad de vida del estudiante, se necesitará ingresar el hechizo Patronus varias veces para vencer al Dementor. 
+ * Si el estudiante no ingresa el hechizo correctamente, perderá turnos y el Dementor consumirá su felicidad y recuerdos felices, acercándose más a la derrota del estudiante. 
+ * En otro caso si el estudiante ingresa bien los hechizos y logra vencer al dementor entonces se mostrar un mensaje de victoria
  * 
  * Vida maxima del dementor: 1000
  * Vida maxima del estudiante: 350
@@ -12,8 +17,35 @@ const leer = require("prompt-sync")();
  */
 
 
+const VIDA_MAXIMA_DEMENTOR = 1000;
+const VIDA_MAXIMA_ESTUDIANTE = 350;
+const DAÑO_POR_TURNO_PERDIDO = 75;
+const DAÑO_POR_HECHIZO_ACERTADO = 267;
+
 function main() {
+
+let NombreDelHechizo = 0;
+let vidaActualDementor = VIDA_MAXIMA_DEMENTOR;
+let vidaActualEstudiante = VIDA_MAXIMA_ESTUDIANTE;
+
+for (let contador = 0; contador < 5; contador++) {
+    console.log("Ingrese el nombre del Hechizo");
+    NombreDelHechizo = leer();
+
+    if (NombreDelHechizo == "Patronus") {
+        vidaActualDementor = vidaActualDementor - DAÑO_POR_HECHIZO_ACERTADO;
+        console.log("Le hiciste daño al dementor");
+        console.log("La vida de dementor es", vidaActualDementor);
+    } else {
+        vidaActualEstudiante = vidaActualEstudiante - DAÑO_POR_TURNO_PERDIDO;
+        console.log("No le hiciste daño al dementor");
+        console.log("Tu vida es", vidaActualEstudiante );
+    }
     
+}
+
+
+
 }
 
 
